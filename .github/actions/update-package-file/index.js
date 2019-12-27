a4bbaaddd934fef792c760d0aee8c8d095f4e8c7
RDegnen <ross@biobright.com>
2019-12-26 20:59:15 -0500

    that worked

--- .github/actions/update-package-file/index.js
+++ .github/actions/update-package-file/index.js
@@ -24,11 +24,7 @@ async function run() {
     )
     const packageObj = JSON.parse(await promisifyCallback(fs.readFile, packageFilePath))
     packageObj.version = process.env.tag
-    const jsonPackage = JSON.stringify(packageObj)
-    //console.log(email)
-    //console.log(context.repo)
-    // To commit an update directly to a branch, might not need to checkout the release branch before this.
-    // https://octokit.github.io/rest.js/#octokit-routes-repos-create-or-update-file
+    const jsonPackage = JSON.stringify(packageObj, undefined, 2)
 
     const { data: { sha } } = await octokit.repos.getContents({
       owner,
