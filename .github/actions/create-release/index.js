fa71a778d06aa7f6322ee5a9942d79e7f20ba120
RDegnen <ross@biobright.com>
2020-01-02 10:20:22 -0500

    asfew

--- .github/actions/create-release/index.js
+++ .github/actions/create-release/index.js
@@ -3,7 +3,14 @@ const { context, GitHub } = require('@actions/github')
 
 function run() {
   try {
-    console.log(JSON.stringify(context.payload, undefined, 2))
+    // console.log(JSON.stringify(context.payload, undefined, 2))
+    // payload has a merged flag that can be used to determine if the closed request was actually merged
+    // can also get merged branch from payload to get the tag
+    const octokit = new GitHub(process.env.GITHUB_TOKEN)
+    const { merged, head } = context.payload
+    console.log(merged, head)
+
+    // use https://octokit.github.io/rest.js/#octokit-routes-repos-create-release to create a tag
   } catch (err) {
     core.setFailed(err)
   }
