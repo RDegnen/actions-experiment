d6a3707291980b101f35f28681b83d7b036dcda7
RDegnen <ross@biobright.com>
2020-01-02 10:50:56 -0500

    tag test

--- .github/actions/create-release/index.js
+++ .github/actions/create-release/index.js
@@ -7,10 +7,19 @@ function run() {
     // payload has a merged flag that can be used to determine if the closed request was actually merged
     // can also get merged branch from payload to get the tag
     const octokit = new GitHub(process.env.GITHUB_TOKEN)
+    const { owner, repo } = context.repo
     const { merged, head } = context.payload.pull_request
-    console.log(merged, head)
+    const { ref } = head
 
     // use https://octokit.github.io/rest.js/#octokit-routes-repos-create-release to create a tag
+    if (merged) {
+      octokit.repos.createRelease({
+        owner,
+        repo,
+        tag_name: ref.split('-')[1],
+        target_commitish: 'master'
+      })
+    }
   } catch (err) {
     core.setFailed(err)
   }
